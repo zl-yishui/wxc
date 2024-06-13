a360f96baa51a7e6160c2604f99f73d7fe20b858
zl-yishui <zhaoleihai@126.com>
2024-06-13 17:24:27 +0800

    位于分支 master 您的分支与上游分支 'origin/master' 一致。 要提交的变更： 修改：     wxcloudrun/models.py

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -26,9 +26,9 @@ class Xueye( models.Model ):
     xueye_id = models.CharField( max_length=64, primary_key=True, verbose_name="学业编号" )
     xueye_xueyuan_name = models.CharField( max_length=15, blank=True, default='', verbose_name="报名姓名" )
     xueye_xueyuan_shenfen = models.CharField( max_length=20, blank=False, default='371323190000000000', verbose_name="身份证号" )
-    xueye_banji = models.ForeignKey( to='Banji', on_delete = models.CASCADE, verbose_name="班级编号" )
+    xueye_banji = models.IntegerField( max_length=8, default=99, verbose_name="班级编号" )
     xueye_banji_name = models.CharField( max_length=150, blank=True, default='', verbose_name="班级名称" )
-    zhaoshengjihua = models.ForeignKey( to='Zhaoshengjihua', default=99999999, on_delete = models.CASCADE, verbose_name="招生计划" )
+    zhaoshengjihua = models.IntegerField( max_length=8, default=99999999, verbose_name="招生计划" )
     chengji = models.SmallIntegerField( default=0, verbose_name="成绩" )
     zhuangtai = models.SmallIntegerField( choices = ZHUANGTAI_OPTION, default=0, verbose_name="状态" )
 
