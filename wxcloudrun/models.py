28a34bd2f241ef73482bc605fd23f5b6d71f423c
zl-yishui <zhaoleihai@126.com>
2024-06-14 17:05:59 +0800

    要提交的变更： 修改：     wxcloudrun/models.py

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Counters(models.Model):
     id = models.AutoField
-    count = models.IntegerField(max_length=11, default=0)
+    count = models.IntegerField(default=0)
     createdAt = models.DateTimeField(default=datetime.now(), )
     updatedAt = models.DateTimeField(default=datetime.now(),)
 
@@ -26,9 +26,9 @@ class Xueye( models.Model ):
     xueye_id = models.CharField( max_length=64, primary_key=True, verbose_name="学业编号" )
     xueye_xueyuan_name = models.CharField( max_length=15, blank=True, default='', verbose_name="报名姓名" )
     xueye_xueyuan_shenfen = models.CharField( max_length=20, blank=False, default='371323190000000000', verbose_name="身份证号" )
-    xueye_banji = models.IntegerField( max_length=8, default=99, verbose_name="班级编号" )
+    xueye_banji = models.IntegerField( default=99, verbose_name="班级编号" )
     xueye_banji_name = models.CharField( max_length=150, blank=True, default='', verbose_name="班级名称" )
-    zhaoshengjihua = models.IntegerField( max_length=8, default=99999999, verbose_name="招生计划" )
+    zhaoshengjihua = models.IntegerField( default=99999999, verbose_name="招生计划" )
     chengji = models.SmallIntegerField( default=0, verbose_name="成绩" )
     zhuangtai = models.SmallIntegerField( choices = ZHUANGTAI_OPTION, default=0, verbose_name="状态" )
 
@@ -38,3 +38,4 @@ class Xueye( models.Model ):
     class Meta:
         ordering = ['xueye_id']
         verbose_name="xueye"
+        db_table = 'wxc_xueye'  # 数据库表名
